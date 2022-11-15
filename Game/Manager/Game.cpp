5edafc3809bf67fd41e75b3a502ecfbedb6e46a4
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-11-15 18:24:48 +0900

    create Game

---
+++ Game/Manager/Game.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Game.h"
+
+void Game::Start()
+{
+    GetSceneManager;
+}
+
+bool Game::Update()
+{
+    if (GetSceneManager->GetNowScene()->Update()) return true;
+
+    return false;
+}
+
+void Game::End()
+{ GetSceneManager->DeleteSingleton(); }
+
+Engine::Game* Engine::Initialize()
+{ return new ::Game(); }
